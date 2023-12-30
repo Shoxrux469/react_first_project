d3ea20feaaff8af831456fe42fa24676934b6d3c
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-12-30 17:58:37 +0500

    fixed

---
+++ src/components/employers-list/employers-list-item/employers-list-item.js
@@ -0,0 +1,61 @@
+import { Component } from "react";
+import "./employers-list-item.css";
+
+class EmployersListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      like: false,
+    };
+  }
+  onIncrease = () => {
+    this.setState(({ increase }) => ({
+      increase: !increase,
+    }));
+  };
+  addLike = () => {
+    this.setState(({ like }) => ({
+      like: !like,
+    }));
+  };
+  render() {
+    const { name, salary } = this.props;
+    const { increase, like } = this.state;
+    let className = "list-group-item d-flex justify-content-between";
+    if (increase) {
+      className += " increase";
+    }
+    if (like) {
+      className += " like";
+    }
+    return (
+      <li className={className}>
+        <span onClick={this.addLike} className="list-group-item-label">
+          {name}
+        </span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + "$"}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            onClick={this.onIncrease}
+            className="btn-cookie btn-sm "
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm ">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default EmployersListItem;
